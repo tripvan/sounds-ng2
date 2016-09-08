f7f0eabd5bb0c90cff781625839c509a23cece0a
Tristan Channing <Tristan Channing>
2016-09-08 22:38:59 +0100

    all routes to index.html

--- server/index.js
+++ server/index.js
@@ -9,19 +9,19 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(staticRoot));
 
-app.use(function(req, res, next){
+app.use('*', function(req, res, next){
 
     // if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if(accept !== 'html'){
-        return next();
-    }
+    // var accept = req.accepts('html', 'json', 'xml');
+    // if(accept !== 'html'){
+    //     return next();
+    // }
 
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== ''){
-        return next();
-    }
+    // // if the request has a '.' assume that it's for a file, move along
+    // var ext = path.extname(req.path);
+    // if (ext !== ''){
+    //     return next();
+    // }
 
     fs.createReadStream(staticRoot + 'index.html').pipe(res);
 
