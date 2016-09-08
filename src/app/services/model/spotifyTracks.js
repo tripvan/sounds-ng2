f6cdd483f7d2899b8fb5701086b00cb0941c8ee2
Tristan Channing <Tristan Channing>
2016-09-08 08:43:56 +0100

    remove the dev files

--- src/app/services/model/spotifyTracks.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var Tracks = (function () {
-    function Tracks(items) {
-        this.items = items;
-    }
-    return Tracks;
-}());
-exports.Tracks = Tracks;
-//# sourceMappingURL=spotifyTracks.js.map
