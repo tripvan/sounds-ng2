26af5df8f69a61e8b46cdbe5eb61aedc055f74c5
Tristan Channing <Tristan Channing>
2016-09-09 08:20:20 +0100

    express route catch all

--- server/index.js
+++ server/index.js
@@ -9,21 +9,21 @@ app.set('port', (process.env.PORT || 3000));
 
 app.use(express.static(staticRoot));
 
-app.use(function(req, res, next){
-
+app.use('*', function(req, res, next){
+    res.sendFile(staticRoot + 'index.html')
     // if the request is not html then move along
-    var accept = req.accepts('html', 'json', 'xml');
-    if(accept !== 'html'){
-        return next();
-    }
+    // var accept = req.accepts('html', 'json', 'xml');
+    // if(accept !== 'html'){
+    //     return next();
+    // }
 
-    // if the request has a '.' assume that it's for a file, move along
-    var ext = path.extname(req.path);
-    if (ext !== ''){
-        return next();
-    }
+    // // if the request has a '.' assume that it's for a file, move along
+    // var ext = path.extname(req.path);
+    // if (ext !== ''){
+    //     return next();
+    // }
 
-    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+    // fs.createReadStream(staticRoot + 'index.html').pipe(res);
 });
 
 app.listen(app.get('port'), function() {
