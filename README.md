252e3eecc8ee46fd9b33a553debfc7ae86a7c0f2
Tristan Channing <Tristan Channing>
2016-09-10 00:17:13 +0100

    remove unnecessary files, webpack prod

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-"# sounds-ng2" 
+Sounds Ng2
+=========
+
+Sounds Ng2 is an Angular 2 web application written in TypeScript, built with webpack, and hosted on [Azure](http://sounds-ng2.azurewebsites.net).
+
+## Purpose
+
+Learning Angular 2, TypeScript, webpack, and Azure. 
+
+## The Website
+
+A tool to find new music using the Spotify Api search endpoints not exposed in their applications such as label and year search.  
---
+++ webpack.common.js
@@ -0,0 +1,244 @@
+/**
+ * @author: @AngularClass
+ */
+
+const webpack = require('webpack');
+const path = require('path');
+
+// const helpers = require('./helpers');
+
+/*
+ * Webpack Plugins
+ */
+// problem with copy-webpack-plugin
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkCheckerPlugin = require('awesome-typescript-loader').ForkCheckerPlugin;
+
+/*
+ * Webpack Constants
+ */
+const METADATA = {
+  title: 'Angular2 Webpack Starter by @gdi2290 from @AngularClass',
+  baseUrl: '/',
+  isDevServer: true,
+  host: 'localhost',
+  port: 3000,
+  ENV: 'development',
+  HMR: true
+};
+
+
+/*
+ * Webpack configuration
+ *
+ * See: http://webpack.github.io/docs/configuration.html#cli
+ */
+module.exports = {
+
+  /*
+   * Static metadata for index.html
+   *
+   * See: (custom attribute)
+   */
+  metadata: METADATA,
+  debug: true,
+  devtool: 'cheap-module-source-map',
+
+  /*
+   * Cache generated modules and chunks to improve performance for multiple incremental builds.
+   * This is enabled by default in watch mode.
+   * You can pass false to disable it.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#cache
+   */
+   //cache: false,
+
+  /*
+   * The entry point for the bundle
+   * Our Angular.js app
+   *
+   * See: http://webpack.github.io/docs/configuration.html#entry
+   */
+  entry: {
+
+    'polyfills': './src/polyfills.ts',
+    'vendor':    './src/vendor.ts',
+    'main':      './src/main.ts'
+
+  },
+
+  /**
+   * Options affecting the output of the compilation.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#output
+   */
+  output: {
+
+    /**
+     * The output directory as absolute path (required).
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-path
+     */
+    path: 'dist',
+
+    /**
+     * Specifies the name of each output file on disk.
+     * IMPORTANT: You must not specify an absolute path here!
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-filename
+     */
+    filename: '[name].bundle.js',
+
+    /**
+     * The filename of the SourceMaps for the JavaScript files.
+     * They are inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-sourcemapfilename
+     */
+    sourceMapFilename: '[name].map',
+
+    /** The filename of non-entry chunks as relative path
+     * inside the output.path directory.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#output-chunkfilename
+     */
+    chunkFilename: '[id].chunk.js'
+
+  },
+
+  /*
+   * Options affecting the resolving of modules.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#resolve
+   */
+  resolve: {
+
+    /*
+     * An array of extensions that should be used to resolve modules.
+     *
+     * See: http://webpack.github.io/docs/configuration.html#resolve-extensions
+     */
+    extensions: ['', '.ts', '.js', '.json'],
+
+    // Make sure root is src
+    root: 'src',
+
+    // remove other default values
+    modulesDirectories: ['node_modules'],
+  },
+
+  /*
+   * Options affecting the normal modules.
+   *
+   * See: http://webpack.github.io/docs/configuration.html#module
+   */
+  module: {
+    loaders: [
+        {
+            test: /\.ts$/,
+            loader: 'awesome-typescript-loader',
+            exclude: [/\.(spec|e2e)\.ts$/]
+        },
+        {
+            test: /\.json$/,
+            loader: 'json-loader'
+        },
+        {
+            test: /\.css$/,
+            loaders: ['to-string-loader', 'css-loader']
+        },
+
+        {
+            test: /\.less$/,
+            loader: 'style!css!less',
+            include: path.resolve(__dirname, "node_modules/bootstrap/less")
+            // Or
+            // loaders: ['style', 'css', 'sass'],
+        },
+        {
+            test: /\.html$/,
+            loader: 'raw-loader',
+        },
+        {
+            test: /\.(jpeg|png|gif|svg)$/i,
+            exclude: /node_modules/,
+            loader:'url-loader?limit=10000'
+        }, 
+        {
+            test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/font-woff"
+        }, 
+        {
+            test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/font-woff"
+        }, 
+        {
+            test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=application/octet-stream"
+        },
+        {
+            test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "file"
+        },
+        {
+            test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+            loader: "url?limit=10000&mimetype=image/svg+xml"
+        }
+    ]
+  },
+  plugins: [
+    new ForkCheckerPlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: ['polyfills', 'vendor'].reverse()
+    }),
+    new CopyWebpackPlugin([
+      {
+        from: 'server',
+        to: ''
+      },
+      {
+        from: 'web.config',
+        to: ''
+      }
+    ]),
+    new HtmlWebpackPlugin({
+      template: 'src/index.ejs',
+      chunksSortMode: 'dependency'
+    })
+  ],
+  
+  /**
+   * Webpack Development Server configuration
+   * Description: The webpack-dev-server is a little node.js Express server.
+   * The server emits information about the compilation state to the client,
+   * which reacts to those events.
+   *
+   * See: https://webpack.github.io/docs/webpack-dev-server.html
+   */
+  devServer: {
+    port: METADATA.port,
+    host: METADATA.host,
+    historyApiFallback: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },
+    outputPath: "dist"
+  },
+  /*
+   * Include polyfills or mocks for various node stuff
+   * Description: Node configuration
+   *
+   * See: https://webpack.github.io/docs/configuration.html#node
+   */
+  node: {
+    global: 'window',
+    crypto: 'empty',
+    module: false,
+    clearImmediate: false,
+    setImmediate: false
+  }
+
+};
