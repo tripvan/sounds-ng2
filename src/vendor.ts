2e48ec73aa73bce57ca7166517cea568316b0480
Tristan Channing <Tristan Channing>
2017-02-04 12:49:34 +0000

    import used bits of rxjs

--- src/vendor.ts
+++ src/vendor.ts
@@ -7,8 +7,16 @@ import "@angular/router";
 // import "@angular/router-deprecated";
 
 // RxJS
-import "rxjs";
-// import "rxjs/add/operator/map";
-// import "rxjs/add/operator/mergeMap";
+import { Subject } from "rxjs/Subject";
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from "rxjs/BehaviorSubject";
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/concatAll';
+import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/from';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/filter';
 
 require("bootstrap/less/bootstrap.less");
