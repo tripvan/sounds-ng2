cf2faf0eb7cd3c2bd77bb82e6f1ff00feb51acbb
Tristan Channing <Tristan Channing>
2017-02-22 08:57:19 +0000

    years & labels scrollable

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
-
+import { PerfectScrollbarConfigInterface, PerfectScrollbarModule } from 'angular2-perfect-scrollbar';
+const PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true
+};
 import { HeaderComponent } from './header.component';
 import { YearsService } from '../services/years.service';
 import { LabelService } from '../services/label.service';
 @NgModule({
-    imports: [SharedModule, FormsModule],
+    imports: [SharedModule, FormsModule, PerfectScrollbarModule.forRoot(PERFECT_SCROLLBAR_CONFIG)],
     declarations: [HeaderComponent],
     exports: [HeaderComponent],
     providers: [YearsService, LabelService]
