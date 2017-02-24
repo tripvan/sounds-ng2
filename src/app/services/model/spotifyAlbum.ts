6a5e467bf8c4e3f79ea05ea0203c3b835067962b
Tristan Channing <Tristan Channing>
2017-02-24 22:04:29 +0000

    ie popularity styling fix

--- src/app/services/model/spotifyAlbum.ts
+++ src/app/services/model/spotifyAlbum.ts
@@ -26,8 +26,21 @@ export class SpotifyAlbum {
         public copyrights: SpotifyCopyright[],
         public popularity: string,
         public trustedUri: SafeUrl) {
-            this.popularity = String((+this.popularity / 10));
+            this.popularity = String(this._getPopularity(popularity));
             this.image = images[images.length - 1];
             this.copyright = copyrights && copyrights.length > 0 ? copyrights[0] : null;
+        }
+
+        private _getPopularity(popularity: string) {
+          let popularityNumber = +popularity / 10;
+          // i.e. styling goes mental with these values. 
+          if (popularityNumber === 0) {
+            return 0.1;
+          }
+          if (popularityNumber === 0.8) {
+            return 0.7;
+          }
+
+          return popularityNumber;
         }
 }
