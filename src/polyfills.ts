e78f7b9d1b3e005ebacdd3414ce6ad57fc452925
Tristan Channing <Tristan Channing>
2017-05-27 16:57:14 +0100

    chore(packages): remove unused packages

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -28,8 +28,5 @@ import "core-js/es6/reflect";
 import "core-js/es7/reflect";
 import "zone.js/dist/zone";
 
-// Typescript emit helpers polyfill
-import "ts-helpers";
-
 import 'web-animations-js';
 
